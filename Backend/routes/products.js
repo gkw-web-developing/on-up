2938cd67b6f588ebf446265745f3397c93fa5350
Pinkify_ <huelleigor@gmail.com>
2026-01-08 14:24:28 +0100

    Merged Backend from JS to TS

--- Backend/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/:id", (req, res) => {
-  res.status(200);
-  res.send({ "Cukiernikkk": req.params.id });
-});
-
-module.exports = router;
